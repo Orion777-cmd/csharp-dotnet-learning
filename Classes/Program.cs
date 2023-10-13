8ff7f3b2ac3b78ec4d5e7d975a69ae3e887a9ebe
Orion777-cmd <abiy.biru78@gmail.com>
2023-10-13 16:34:43 +0300

    learned how classes work on c#

---
+++ Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            myCar.Make = "Toyota";
+            myCar.Model = "Camry";
+            myCar.Year = 2019;
+            myCar.Color = "Black";
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make,
+                myCar.Model,
+                myCar.Year,
+                myCar.Color
+            );
+
+            Console.ReadLine();
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+    }
+}
