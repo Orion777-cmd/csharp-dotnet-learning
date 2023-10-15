41e8d6e2102e1499be0b7748be1996602a9bc195
Orion777-cmd <abiy.biru78@gmail.com>
2023-10-16 00:15:12 +0300

    learned the use and syntax of enums and switches

---
+++ EnumsAndSwitch/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnumsAndSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Enums
+            // Enums are strongly typed constants
+            // if you want a variable that can only have certain values
+            // by default enums are integers starting at 0
+            // you can change the start value if you want
+            // you can also assign the value of each enum
+            // if you don't assign values they will be assigned automatically
+            // you can also use enums inside a switch statement
+            // enums are value types
+            // enums are great for representing state machines
+            // enums are great for representing options, choices, menus, etc
+
+            List<Todo> todos = new List<Todo>()
+            {
+                new Todo { Description = "Task 1", EstimatedHours = 6, Status = Status.Completed },
+                new Todo { Description = "Task 2", EstimatedHours = 2, Status = Status.InProgress },
+                new Todo { Description = "Task 3", EstimatedHours = 8, Status = Status.NotStarted },
+                new Todo { Description = "Task 4", EstimatedHours = 12, Status = Status.Deleted },
+                new Todo { Description = "Task 5", EstimatedHours = 6, Status = Status.NotStarted },
+                new Todo { Description = "Task 6", EstimatedHours = 2, Status = Status.InProgress },
+                new Todo { Description = "Task 7", EstimatedHours = 14, Status = Status.NotStarted },
+                new Todo { Description = "Task 8", EstimatedHours = 8, Status = Status.Completed },
+                new Todo { Description = "Task 9", EstimatedHours = 8, Status = Status.Completed },
+                new Todo { Description = "Task 10", EstimatedHours = 8, Status = Status.Completed },
+                new Todo { Description = "Task 11", EstimatedHours = 4, Status = Status.Completed },
+                new Todo { Description = "Task 12", EstimatedHours = 10, Status = Status.Completed },
+                new Todo { Description = "Task 13", EstimatedHours = 12, Status = Status.Deleted },
+                new Todo { Description = "Task 14", EstimatedHours = 6, Status = Status.Completed },
+            };
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            // Call the printAssessment method to display the tasks
+            printAssessment(todos);
+        }
+
+        private static void printAssessment(List<Todo> todos)
+        {
+            foreach (var todo in todos)
+            {
+                switch (todo.Status)
+                {
+                    case Status.NotStarted:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    case Status.InProgress:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case Status.OnHold:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        break;
+                    case Status.Completed:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                    case Status.Deleted:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine("Description: {0}", todo.Description);
+                
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Todo
+    {
+        public string Description { get; set; }
+        public int EstimatedHours { get; set; }
+        public Status Status { get; set; }
+    }
+
+    enum Status
+    {
+        NotStarted,
+        InProgress,
+        OnHold,
+        Completed,
+        Deleted
+    }
+}
