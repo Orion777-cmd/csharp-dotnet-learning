a00d3e2247ff59acede93e8a6901b57e6d40915d
Orion777-cmd <abiy.biru78@gmail.com>
2023-10-15 21:48:30 +0300

    delve into more about classes

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -7,6 +7,12 @@ namespace Classes
         static void Main(string[] args)
         {
             Car myCar = new Car();
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make,
+                myCar.Model,
+                myCar.Year,
+                myCar.Color
+            );
 
             myCar.Make = "Toyota";
             myCar.Model = "Camry";
@@ -21,6 +27,9 @@ namespace Classes
             );
 
             Console.ReadLine();
+
+            // to create methods with out creating an instance of the class
+            // we use static keyword
         }
     }
 
@@ -30,5 +39,25 @@ namespace Classes
         public string Model { get; set; }
         public int Year { get; set; }
         public string Color { get; set; }
+
+        public Car(){
+            this.Make = "Nissan";
+            this.Model = "Altima";
+            this.Year = 2018;
+            this.Color = "White";
+
+        }
+
+        //overloading
+        public Car(string make, string model, int year, string color){
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.Color = color;
+        }
+
+        public static void myMethod(){
+            Console.WriteLine("Called the static myMethod");
+        }
     }
 }
